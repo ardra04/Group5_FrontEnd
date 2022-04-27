5ab2c3a7ea8d1d1bf17204ba6e7c441d3a67b6d8
vkdarsith <vkdarsith@gmail.com>
2022-04-27 12:35:01 +0530

    Adding Usermanagement module

---
+++ Usermanagement_module/src/App.js
@@ -0,0 +1,28 @@
+import Topbar from "./components/topbar/Topbar";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import UserList from "./pages/userList/UserList";
+import User from "./pages/user/User";
+
+function App() {
+  return (
+    <Router>
+      <Topbar />
+      <div className="container">
+        <Switch>
+          <Route exact path="/">
+            <UserList />
+          </Route>
+          <Route path="/users">
+            <UserList />
+          </Route>
+          <Route path="/user/:userId">
+            <User />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
