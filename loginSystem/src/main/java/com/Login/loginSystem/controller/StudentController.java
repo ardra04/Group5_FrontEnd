fbe3e73cdcc143bbfca94f46215ec7860a962166
Dhanyajr <dhanyakrishnan5gold.com>
2022-04-27 11:47:28 +0530

    Project update

---
+++ loginSystem/src/main/java/com/Login/loginSystem/controller/StudentController.java
@@ -0,0 +1,28 @@
+package com.Login.loginSystem.controller;
+import com.Login.loginSystem.model.Student;
+import com.Login.loginSystem.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+//
+// import org.springframework.beans.factory.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/student")
+@CrossOrigin
+public class StudentController {
+    @Autowired
+    private StudentService studentService;
+
+    @PostMapping("/add")
+    public String add(@RequestBody Student student){
+        studentService.saveStudent(student);
+        return "New user login details are added";
+    }
+
+    @GetMapping("/getAll")
+    public List<Student> list(){
+        return studentService.getAllStudents();
+    }
+}
---
+++ loginSystem/src/main/java/com/Login/loginSystem/repository/StudentRepository.java
@@ -0,0 +1,8 @@
+package com.Login.loginSystem.repository;
+import com.Login.loginSystem.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student,Integer> {
+}
