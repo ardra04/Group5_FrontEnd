f28733c4a793514ff1af8dc699d39d4ae237b388
venkatasriram <70480172+venkatasriram@users.noreply.github.com>
2022-04-29 09:31:44 +0530

    Delete loginSystem directory

--- loginSystem/src/main/java/com/Login/loginSystem/repository/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.Login.loginSystem.repository;
-import com.Login.loginSystem.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudentRepository extends JpaRepository<Student,Integer> {
-}
